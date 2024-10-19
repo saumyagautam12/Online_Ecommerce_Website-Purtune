b0900fc36dcd0d5e3fe85afba44cd5fcdda103db
saumya gautam <saumyagautam2000@gmail.com>
2024-10-20 00:56:35 +0530

    entries of test data added

--- core/views.py
+++ core/views.py
@@ -181,7 +181,7 @@ class Login(LoginView):
 
 # @unauthenticated_user
 # def login(request):
-#     # returnUrl=request.GET.get('return_url')          -------- land with the same earlier url which was before after login 
+#     # returnUrl=request.GET.get('return_url')        
 #     if request.method=="POST":
 #         # name=request.POST.get('name')
 #         username=request.POST.get('username')
