0c836daeee8079f43d30006876b722dd44ac4d75
saumya gautam <saumyagautam2000@gmail.com>
2024-10-19 14:40:54 +0530

    order style

--- core/views.py
+++ core/views.py
@@ -181,7 +181,7 @@ class Login(LoginView):
 
 # @unauthenticated_user
 # def login(request):
-#     # returnUrl=request.GET.get('return_url')
+#     # returnUrl=request.GET.get('return_url')          -------- land with the same earlier url which was before after login 
 #     if request.method=="POST":
 #         # name=request.POST.get('name')
 #         username=request.POST.get('username')
